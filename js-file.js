fcdb0f972779b25e7a99bea1b087d5a80c419084
WildAxewAppears <a.dafflitto@virgilio.it>
2024-06-05 20:42:12 +0200

    now operations are displayed on screen and text resize accordingly to fit the screen

--- js-file.js
+++ js-file.js
@@ -8,6 +8,19 @@ let resultCalculated = false;
 let lastOperation = "";
 let lastNumber = ""
 
+function updateMonitor() {
+    let text = monitor.value;
+    monitor.classList.remove('small-text', 'smaller-text', 'smallest-text');
+
+    if (text.length > 10 && text.length <= 15) {
+        monitor.classList.add('small-text');
+    } else if (text.length > 15 && text.length <= 20) {
+        monitor.classList.add('smaller-text');
+    } else if (text.length > 20) {
+        monitor.classList.add('smallest-text');
+    }
+}
+
 const numbers = document.querySelectorAll("[data-number]");
 numbers.forEach((number) => {
     number.addEventListener("click", () => {
@@ -15,27 +28,26 @@ numbers.forEach((number) => {
         if (resultCalculated && operator == "") {
             reset();
         }
-        if (!resultCalculated && firstNumber.length < 16) {
+        if (!resultCalculated && firstNumber.length < 15) {
             if (operator === "") {
-                
                 if (firstNumber === "0") {
                     firstNumber = firstNumber.slice(1); 
                 }
                 firstNumber += num;
                 monitor.value = firstNumber;
+                updateMonitor();
             }
         }
-        if (operator !== "" && secondNumber.length < 16) {
-           
+        if (operator !== "" && secondNumber.length < 15) {
             if (secondNumber === "0") {
                 secondNumber = secondNumber.slice(1); 
             }
             secondNumber += num;
-            monitor.value = secondNumber;
+            monitor.value = `${firstNumber} ${operator} ${secondNumber}`;
+            updateMonitor();
         }
     });
 });
-
 
 const operators = document.querySelectorAll("[data-operator]");
 operators.forEach((operatorButton) => {
@@ -43,13 +55,17 @@ operators.forEach((operatorButton) => {
         const op = operatorButton.getAttribute('data-operator');
         if (firstNumber !== "" && operator !== "" && secondNumber !== "") {
             operate();
-            lastOperation = ""
-            lastNumber = ""
+            lastOperation = "";
+            lastNumber = "";
             operator = op;
+            monitor.value = `${firstNumber} ${operator}`;
+            updateMonitor();
         } else if (firstNumber !== "") {
-            lastOperation = ""
-            lastNumber = ""
+            lastOperation = "";
+            lastNumber = "";
             operator = op;
+            monitor.value = `${firstNumber} ${operator}`;
+            updateMonitor();
         }
     });
 });
@@ -61,11 +77,13 @@ decimal.addEventListener('click', () => {
             if (!firstNumber.includes('.')) {
                 firstNumber += '.';
                 monitor.value = firstNumber;
+                updateMonitor();
             }
         } else {
             if (!secondNumber.includes('.') && secondNumber !== "") {
                 secondNumber += '.';
-                monitor.value = secondNumber;
+                monitor.value = `${firstNumber} ${operator} ${secondNumber}`;
+                updateMonitor();
             }
         }
     }
@@ -78,10 +96,10 @@ equal.addEventListener("click", () => {
         firstNumber = monitor.value;
         operator = lastOperation;
         operate();
+    } else {
+        operate();
     }
-    else {
-        operate()
-    }
+    updateMonitor();
 });
 
 function operate() {
@@ -124,24 +142,25 @@ function operate() {
     }
     firstNumber = (parseFloat(result)).toString();
     monitor.value = firstNumber;
+    updateMonitor();
 
     lastOperation = operator;
-    lastNumber = secondNumber
+    lastNumber = secondNumber;
 
     operator = "";
     secondNumber = "";
     resultCalculated = true;
 }
 
-
 function reset() {
-    lastOperation = ""
-    lastNumber = ""
+    lastOperation = "";
+    lastNumber = "";
     resultCalculated = false;
     firstNumber = "";
     operator = "";
     secondNumber = "";
     monitor.value = firstNumber;
+    updateMonitor();
 }
 
 const ac = document.querySelector("#reset");
@@ -153,11 +172,13 @@ del.addEventListener("click", function() {
         if (operator === "" && firstNumber !== "") {
             firstNumber = firstNumber.slice(0, -1);
             monitor.value = firstNumber;
+            updateMonitor();
         }
     }
     if (operator !== "" && secondNumber !== "") {
         secondNumber = secondNumber.slice(0, -1);
-        monitor.value = secondNumber;
+        monitor.value = `${firstNumber} ${operator} ${secondNumber}`;
+        updateMonitor();
     }
 });
 
@@ -175,6 +196,7 @@ document.addEventListener('keydown', (event) => {
                 }
                 firstNumber += num;
                 monitor.value = firstNumber;
+                updateMonitor();
             }
         }
         if (operator !== "" && secondNumber.length < 16) {
@@ -182,29 +204,36 @@ document.addEventListener('keydown', (event) => {
                 secondNumber = secondNumber.slice(1); 
             }
             secondNumber += num;
-            monitor.value = secondNumber;
+            monitor.value = `${firstNumber} ${operator} ${secondNumber}`;
+            updateMonitor();
         }
     } else if (['+', '-', '*', '/'].includes(key)) {
         if (firstNumber !== "" && operator !== "" && secondNumber !== "") {
             operate();
-            lastOperation = ""
-            lastNumber = ""
+            lastOperation = "";
+            lastNumber = "";
             operator = key;
+            monitor.value = `${firstNumber} ${operator}`;
+            updateMonitor();
         } else if (firstNumber !== "") {
             operator = key;
-            lastOperation = ""
-            lastNumber = ""
+            lastOperation = "";
+            lastNumber = "";
+            monitor.value = `${firstNumber} ${operator}`;
+            updateMonitor();
         }
     } else if (key === '.') {
         if (operator === '') {
             if (!firstNumber.includes('.') && firstNumber !== "") {
                 firstNumber += '.';
                 monitor.value = firstNumber;
+                updateMonitor();
             }
         } else {
             if (!secondNumber.includes('.') && secondNumber !== "") {
                 secondNumber += '.';
-                monitor.value = secondNumber;
+                monitor.value = `${firstNumber} ${operator} ${secondNumber}`;
+                updateMonitor();
             }
         }
     } else if (key === 'Enter') {
@@ -213,20 +242,22 @@ document.addEventListener('keydown', (event) => {
             firstNumber = monitor.value;
             operator = lastOperation;
             operate();
+        } else {
+            operate();
         }
-        else {
-            operate()
-        }
+        updateMonitor();
     } else if (key === 'Backspace') {
         if (!resultCalculated) {
             if (operator === "" && firstNumber !== "") {
                 firstNumber = firstNumber.slice(0, -1);
                 monitor.value = firstNumber;
+                updateMonitor();
             }
         }
         if (operator !== "" && secondNumber !== "") {
             secondNumber = secondNumber.slice(0, -1);
-            monitor.value = secondNumber;
+            monitor.value = `${firstNumber} ${operator} ${secondNumber}`;
+            updateMonitor();
         }
     }
 });
